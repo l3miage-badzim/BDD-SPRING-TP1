cffd7debfef941db41387cdcf2dfb3bbc6f5760e
badzim <badzi.amine@outlook.com>
2024-04-05 00:54:56 +0200

    init server implementation

--- server/src/main/java/fr/uga/l3miage/tp3/exo1/exceptions/rest/AddingSongRestException.java
+++
@@ -1,7 +0,0 @@
-package fr.uga.l3miage.tp3.exo1.exceptions.rest;
-
-public class AddingSongRestException extends RuntimeException {
-    public AddingSongRestException(String message) {
-        super(message);
-    }
-}
