67e49b10fc7d8716d2aabbc2bf942a2ab13dec37
badzim <badzi.amine@outlook.com>
2024-04-05 00:47:52 +0200

    Ajout methode http get et patch + documentation swagger avec ApiResponse de swagger v3

--- rest-api/src/main/java/fr/uga/l3miage/exo1/endpoints/PlaylistEnpoint.java
+++ rest-api/src/main/java/fr/uga/l3miage/exo1/endpoints/PlaylistEnpoint.java
@@ -1,12 +1,27 @@
 package fr.uga.l3miage.exo1.endpoints;
 
+import fr.uga.l3miage.exo1.responses.PlaylistResponseDTO;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name="Gestion playlist", description = "Tous les endpoinds de gestion d'une playlist")
 @RestController
 @RequestMapping("/api/playlist")
 public interface PlaylistEnpoint {
-    //PlaylistResponseDTO addSongInPlaylist(@PathVariable(name = "idPlaylist")String idPlaylist, @RequestParam String idSong);
+
+    @PatchMapping("/{idPlaylist}/add") // exemple d'appel /api/playlist/1/add?idSong=monSong ici idPlaylist = 1 && idSong = "monSong"
+    @ApiResponse(responseCode = "200", description = "Le son à été ajouté à la playlist")
+    @ApiResponse(responseCode = "404", description = "la playlist ou le son demandée n'a pas été trouvé",content = @Content(schema = @Schema(implementation = AddPlaylistErrorResponse.class),mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @ResponseStatus(HttpStatus.OK)
+    PlaylistResponseDTO addSongInPlaylist(@PathVariable(name = "idPlaylist")String idPlaylist, @RequestParam String idSong);
+
+    @PatchMapping("/{idPlaylist}")
+    @ApiResponse(responseCode = "200", description = "Le son à été trouvé")
+    @ResponseStatus(HttpStatus.OK)
+    PlaylistResponseDTO getPlaylist(@PathVariable(name = "idPlaylist")String idPlaylist);    
 }
